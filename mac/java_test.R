6da04dbb7f4d6ef4a6fa908448f29a996f266a8e
Matt Malishev <matthew.malishev@gmail.com>
2018-10-19 14:59:17 -0400

    java test

--- mac/java_test.R
+++ mac/java_test.R
@@ -5,31 +5,31 @@
 
 .jinit()
 .jnew( "java/awt/Point", 10L, 10L )
-f <- .jnew("java/awt/Frame","Hello")
-.jcall(f,,"setVisible",TRUE)
+# f <- .jnew("java/awt/Frame","Hello")
+# .jcall(f,,"setVisible",TRUE)
+# # cat("\n","\n",geterrmessage(),"\n","\n")
+# # stop("Failed Test 1",call.=F)
+# 
+# 
+# # test 2
+# component <- .jnull()
+# component <- .jcast(component, new.class = "java/awt/Component")
+# message <- .jnew("java/lang/String","This is a JOptionPane test from rJava.")
+# message <- .jcast(message, new.class = "java/lang/Object")
+# title <- .jnew("java/lang/String","Test")
+# type <- .jnew("java/lang/Integer", as.integer(2))
+# f <- .jnew("javax/swing/JOptionPane")
+# .jcall(f,"showMessageDialog", component, message, title, .jsimplify(type))
 # cat("\n","\n",geterrmessage(),"\n","\n")
-# stop("Failed Test 1",call.=F)
-
-
-# test 2
-component <- .jnull()
-component <- .jcast(component, new.class = "java/awt/Component")
-message <- .jnew("java/lang/String","This is a JOptionPane test from rJava.")
-message <- .jcast(message, new.class = "java/lang/Object")
-title <- .jnew("java/lang/String","Test")
-type <- .jnew("java/lang/Integer", as.integer(2))
-f <- .jnew("javax/swing/JOptionPane")
-.jcall(f,"showMessageDialog", component, message, title, .jsimplify(type))
-cat("\n","\n",geterrmessage(),"\n","\n")
-stop("Failed Test 2",call.=F)
-
-
-# test 3
-.jcall("java/lang/System", "S", "getProperty", "java.vm.version")
-.jcall("java/lang/System", "S", "getProperty", "java.vm.name")
-.jcall("java/lang/System", "S", "getProperty", "java.vm.info")
-.jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
-.jcall("java/lang/System", "S", "getProperty", "sun.arch.data.model")
+# stop("Failed Test 2",call.=F)
+# 
+# 
+# # test 3
+# .jcall("java/lang/System", "S", "getProperty", "java.vm.version")
+# .jcall("java/lang/System", "S", "getProperty", "java.vm.name")
+# .jcall("java/lang/System", "S", "getProperty", "java.vm.info")
+# .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+# .jcall("java/lang/System", "S", "getProperty", "sun.arch.data.model")
 
 # test 4
 .jcall("java/lang/System", "S", "getProperty", "java.awt.headless")
