c2d1094763f384957f437d77b8d568152d04fdc2
Matt Malishev <matthew.malishev@gmail.com>
2018-10-19 15:15:39 -0400

    java test

--- mac/java_test.R
+++ mac/java_test.R
@@ -1,39 +1,41 @@
-# Diagnostics for testing you have the correct version of Java on your computer. 
-# Tests are sequential (fromhttps://stackoverflow.com/questions/14915898/rnetlogo-function-nlstart-fails-to-launch-gui).     
+# Diagnostics for testing you have the correct version of Java on your computer.
+# Tests are sequential (from https://stackoverflow.com/questions/14915898/rnetlogo-function-nlstart-fails-to-launch-gui).
 
-# test 1  
+# test 1
 
 .jinit()
 .jnew( "java/awt/Point", 10L, 10L )
-# f <- .jnew("java/awt/Frame","Hello")
-# .jcall(f,,"setVisible",TRUE)
-# # cat("\n","\n",geterrmessage(),"\n","\n")
-# # stop("Failed Test 1",call.=F)
-# 
-# 
+f <- .jnew("java/awt/Frame","Hello")
+.jcall(f,,"setVisible",TRUE)
+t1err <- geterrmessage()
+
 # # test 2
-# component <- .jnull()
-# component <- .jcast(component, new.class = "java/awt/Component")
-# message <- .jnew("java/lang/String","This is a JOptionPane test from rJava.")
-# message <- .jcast(message, new.class = "java/lang/Object")
-# title <- .jnew("java/lang/String","Test")
-# type <- .jnew("java/lang/Integer", as.integer(2))
-# f <- .jnew("javax/swing/JOptionPane")
-# .jcall(f,"showMessageDialog", component, message, title, .jsimplify(type))
-# cat("\n","\n",geterrmessage(),"\n","\n")
-# stop("Failed Test 2",call.=F)
-# 
-# 
+component <- .jnull()
+component <- .jcast(component, new.class = "java/awt/Component")
+message <- .jnew("java/lang/String","This is a JOptionPane test from rJava.")
+message <- .jcast(message, new.class = "java/lang/Object")
+title <- .jnew("java/lang/String","Test")
+type <- .jnew("java/lang/Integer", as.integer(2))
+f <- .jnew("javax/swing/JOptionPane")
+.jcall(f,"showMessageDialog", component, message, title, .jsimplify(type))
+t2err <- geterrmessage()
+
 # # test 3
-# .jcall("java/lang/System", "S", "getProperty", "java.vm.version")
-# .jcall("java/lang/System", "S", "getProperty", "java.vm.name")
-# .jcall("java/lang/System", "S", "getProperty", "java.vm.info")
-# .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
-# .jcall("java/lang/System", "S", "getProperty", "sun.arch.data.model")
+.jcall("java/lang/System", "S", "getProperty", "java.vm.version")
+.jcall("java/lang/System", "S", "getProperty", "java.vm.name")
+.jcall("java/lang/System", "S", "getProperty", "java.vm.info")
+.jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+.jcall("java/lang/System", "S", "getProperty", "sun.arch.data.model")
+t3err <- geterrmessage()
 
 # test 4
 .jcall("java/lang/System", "S", "getProperty", "java.awt.headless")
 Sys.getenv("NOAWT")
+t4err <- geterrmessage()
 
+if(geterrmessage()==t1err){stop("Failed Test 1 — Headless exception \n \n Wrong awt GUI support for Java/rJava",call.=F)}
+if(geterrmessage()==t2err){stop("Failed Test 2 — Invalid method name for RcallMethod (unable to open dialog box)",call.=F)}
+if(geterrmessage()==t3err){stop("Failed Test 3 — Old version of Java. \n \n Download latest version \n \n > https://www.oracle.com/technetwork/java/javase/downloads/index-jsp-138363.html.",call.=F)}
+if(geterrmessage()==t4err){stop("Failed Test 4 — ",call.=F)}
 
 
